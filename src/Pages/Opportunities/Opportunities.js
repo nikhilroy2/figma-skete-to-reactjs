927a04623bd874be6ec12fe5cbe75cb858591d0c
Nikhil Roy <nikhilchandraroypoet@gmail.com>
2023-03-12 07:50:25 +0600

    update and finish opportunities page

--- src/Pages/Opportunities/Opportunities.js
+++ src/Pages/Opportunities/Opportunities.js
@@ -1,8 +1,142 @@
 import React, { useState } from 'react';
 import './Opportunities.css';
 function Opportunities(props) {
-    const [investCounter, setInvestCounter] = useState(134)
 
+    const invest_card_object = [
+        {
+            id: 1,
+            title: 'The Intelligence Exchange Ecosystem',
+            counter: 6,
+            object_list: [
+                {
+                    id: 1,
+                    bg: '#C73C3C',
+                    img: require('../../images/logo-frenchfry.png'),
+                    subway: 888,
+                    tiex: '5.2 TIEX',
+                    invest_counter: 134
+                },
+                {
+                    id: 2,
+                    bg: '#9075FF',
+                    img: require('../../images/logo-frenchfry.png'),
+                    subway: 888,
+                    tiex: '5.2 TIEX',
+                    invest_counter: 134
+                },
+                {
+                    id: 3,
+                    bg: '#C73C3C',
+                    img: require('../../images/logo-frenchfry.png'),
+                    subway: 888,
+                    tiex: '5.2 TIEX',
+                    invest_counter: 134
+                },
+                {
+                    id: 4,
+                    bg: '#9075FF',
+                    img: require('../../images/logo-frenchfry.png'),
+                    subway: 888,
+                    tiex: '5.2 TIEX',
+                    invest_counter: 134
+                },
+                {
+                    id: 5,
+                    bg: '#0AA56D',
+                    img: require('../../images/logo-frenchfry.png'),
+                    subway: 888,
+                    tiex: '5.2 TIEX',
+                    invest_counter: 134
+                },
+                {
+                    id: 6,
+                    bg: '#8A8A8A',
+                    img: require('../../images/nft_logo.png'),
+                    subway: 888,
+                    tiex: '5.2 TIEX',
+                    invest_counter: 134
+                },
+                {
+                    id: 7,
+                    bg: '#7C3E1A',
+                    img: require('../../images/blockchain_logo.png'),
+                    subway: 888,
+                    tiex: '5.2 TIEX',
+                    invest_counter: 134
+                },
+                {
+                    id: 8,
+                    bg: '#546A25',
+                    img: require('../../images/metamask.png'),
+                    subway: 888,
+                    tiex: '5.2 TIEX',
+                    invest_counter: 134
+                },
+            ]
+        },
+        {
+            id: 2,
+            title: 'SingularityNET Ecosystem',
+            counter: 3,
+            object_list: [
+                {
+                    id: 1,
+                    bg: '#161616',
+                    img: require('../../images/polygon.png'),
+                    subway: 888,
+                    tiex: '5.2 TIEX',
+                    invest_counter: 134
+                },
+                {
+                    id: 2,
+                    bg: '#014B2C',
+                    img: require('../../images/base.png'),
+                    subway: 888,
+                    tiex: '5.2 TIEX',
+                    invest_counter: 134
+                },
+                {
+                    id: 3,
+                    bg: '#0AA56D',
+                    img: require('../../images/img_51.png'),
+                    subway: 888,
+                    tiex: '5.2 TIEX',
+                    invest_counter: 134
+                },
+            ]
+        },
+        {
+            id: 3,
+            title: 'Ocean Protocol Ecosystem',
+            counter: 4,
+            object_list: [
+                {
+                    id: 1,
+                    bg: '#161616',
+                    img: require('../../images/Protocol.png'),
+                    subway: 888,
+                    tiex: '5.2 TIEX',
+                    invest_counter: 134
+                },
+                {
+                    id: 2,
+                    bg: '#014B2C',
+                    img: require('../../images/base.png'),
+                    subway: 888,
+                    tiex: '5.2 TIEX',
+                    invest_counter: 134
+                },
+                {
+                    id: 3,
+                    bg: '#0AA56D',
+                    img: require('../../images/img_51.png'),
+                    subway: 888,
+                    tiex: '5.2 TIEX',
+                    invest_counter: 134
+                },
+            ]
+        },
+    ]
     return (
         <div id='Opportunities'>
             <div className="page_container">
@@ -10,106 +144,33 @@ function Opportunities(props) {
                     Opportunities<small className="counter">(12)</small>
                 </h2>
 
-                <h3 className="page_sub_header">
-                    The Intelligence Exchange Ecosystem <small className="counter">(6)</small>
-                </h3>
+
 
                 {/* card */}
                 <div className="card_wrapper">
-                    <div className="invest_card">
-                        <div className="invest_card_header" style={{ '--header-bg': '#0AA56D' }}>
-                            <img src={require('../../images/logo-frenchfry.png')} alt="img" />
-                        </div>
-                        <div className="invest_card_body">
-                            <ul>
-                                <li>
-                                    <div className="list_box">
-                                        <h2 style={{ fontSize: '13px' }}>FRENCHFRY (Subway)</h2>
-                                    </div>
-                                    <div className="list_box" style={{ whiteSpace: 'nowrap' }}>
-                                        {investCounter} / 888
-                                    </div>
-                                </li>
-                                <li>
-                                    <div className="list_box">
-                                        <h2 style={{ fontSize: '19px' }}>
-                                            5.2 TIEX
-                                        </h2>
-                                    </div>
-                                    <div className="list_box">
-                                        <div className="counter_wrapper">
-                                            <button className="counter_plus" onClick={() => setInvestCounter(investCounter + 1)}>
-                                                <svg width={8} height={8} viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                    <path d="M8 4H0" stroke="white" strokeWidth={2} />
-                                                    <path d="M4 8L4 0" stroke="white" strokeWidth={2} />
-                                                </svg>
+                    {invest_card_object.map(v => {
+                        return (
+                            <div key={v.id}>
+                                <h3 className="page_sub_header">
+                                    {v.title} <small className="counter">({v.counter})</small>
+                                </h3>
 
-                                            </button>
-                                            <span className="counter_value">{investCounter}</span>
-                                            <button className="counter_minus" onClick={() => setInvestCounter(investCounter - 1)}>
-                                                <svg width={8} height={8} viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                    <path d="M8 4H0" stroke="white" strokeWidth={2} />
-                                                    {/* <path d="M4 8L4 0" stroke="white" strokeWidth={2} /> */}
-                                                </svg>
-                                            </button>
-                                        </div>
-                                    </div>
-                                </li>
-                            </ul>
-                        </div>
-                        <div className="invest_card_footer">
-                            <button className="invest_btn">
-                                Invest
-                            </button>
-                        </div>
-                    </div>
-                    <div className="invest_card">
-                        <div className="invest_card_header" style={{ '--header-bg': '#0AA56D' }}>
-                            <img src={require('../../images/logo-frenchfry.png')} alt="img" />
-                        </div>
-                        <div className="invest_card_body">
-                            <ul>
-                                <li>
-                                    <div className="list_box">
-                                        <h2 style={{ fontSize: '13px' }}>FRENCHFRY (Subway)</h2>
-                                    </div>
-                                    <div className="list_box" style={{ whiteSpace: 'nowrap' }}>
-                                        {investCounter} / 888
-                                    </div>
-                                </li>
-                                <li>
-                                    <div className="list_box">
-                                        <h2 style={{ fontSize: '19px' }}>
-                                            5.2 TIEX
-                                        </h2>
-                                    </div>
-                                    <div className="list_box">
-                                        <div className="counter_wrapper">
-                                            <button className="counter_plus" onClick={() => setInvestCounter(investCounter + 1)}>
-                                                <svg width={8} height={8} viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                    <path d="M8 4H0" stroke="white" strokeWidth={2} />
-                                                    <path d="M4 8L4 0" stroke="white" strokeWidth={2} />
-                                                </svg>
+                                <div className="card_row">
+                                    {v.object_list.map(j => {
+                                        return (
+                                            <InvestCard key={j.id} j={j}></InvestCard>
 
-                                            </button>
-                                            <span className="counter_value">{investCounter}</span>
-                                            <button className="counter_minus" onClick={() => setInvestCounter(investCounter - 1)}>
-                                                <svg width={8} height={8} viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                    <path d="M8 4H0" stroke="white" strokeWidth={2} />
-                                                    {/* <path d="M4 8L4 0" stroke="white" strokeWidth={2} /> */}
-                                                </svg>
-                                            </button>
-                                        </div>
-                                    </div>
-                                </li>
-                            </ul>
-                        </div>
-                        <div className="invest_card_footer">
-                            <button className="invest_btn">
-                                Invest
-                            </button>
-                        </div>
-                    </div>
+                                        )
+                                    })}
+
+
+                                </div>
+                            </div>
+                        )
+                    })}
+
+
+
                 </div>
                 {/* card end*/}
             </div>
@@ -118,3 +179,57 @@ function Opportunities(props) {
 }
 
 export default Opportunities;
+
+
+const InvestCard = ({ j }) => {
+    const [investCounter, setInvestCounter] = useState(j.invest_counter)
+    return (
+        <div key={j.id} className="invest_card">
+            <div className="invest_card_header" style={{ '--header-bg': j.bg }}>
+                <img src={j.img} alt="img" />
+            </div>
+            <div className="invest_card_body">
+                <ul>
+                    <li>
+                        <div className="list_box">
+                            <h2 style={{ fontSize: '13px' }}>FRENCHFRY (Subway)</h2>
+                        </div>
+                        <div className="list_box" style={{ whiteSpace: 'nowrap' }}>
+                            {investCounter} / {j.subway}
+                        </div>
+                    </li>
+                    <li>
+                        <div className="list_box">
+                            <h2 style={{ fontSize: '19px' }}>
+                                {j.tiex}
+                            </h2>
+                        </div>
+                        <div className="list_box">
+                            <div className="counter_wrapper">
+                                <button className="counter_plus" onClick={() => setInvestCounter(investCounter + 1)}>
+                                    <svg width={8} height={8} viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M8 4H0" stroke="white" strokeWidth={2} />
+                                        <path d="M4 8L4 0" stroke="white" strokeWidth={2} />
+                                    </svg>
+
+                                </button>
+                                <span className="counter_value">{investCounter}</span>
+                                <button className="counter_minus" onClick={() => setInvestCounter(investCounter - 1)}>
+                                    <svg width={8} height={8} viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M8 4H0" stroke="white" strokeWidth={2} />
+                                        {/* <path d="M4 8L4 0" stroke="white" strokeWidth={2} /> */}
+                                    </svg>
+                                </button>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <div className="invest_card_footer">
+                <button className="invest_btn">
+                    Invest
+                </button>
+            </div>
+        </div>
+    )
+}
