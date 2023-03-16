91a6532e0d16e6757921996cdb1d9667ceb87924
Nikhil Roy <nikhilchandraroypoet@gmail.com>
2023-03-16 12:03:00 +0600

    update

--- src/Pages/CreateMarket/CreateMarket.js
+++ src/Pages/CreateMarket/CreateMarket.js
@@ -1,9 +1,50 @@
 import React from 'react';
 import './CreateMarket.css';
+import { Link } from 'react-router-dom';
 function CreateMarket(props) {
+    const links_object = [
+        {
+            id: 1,
+            name: 'Dashboard',
+            path: '/'
+        },
+        {
+            id: 2,
+            name: 'My Models',
+            path: '/my_models'
+        },
+        {
+            id: 3,
+            name: 'My Shares',
+            path: '/launch_share'
+        },
+        {
+            id: 4,
+            name: 'For businesses',
+            path: '/for_businesses'
+        },
+        {
+            id: 5,
+            name: 'For investors',
+            path: '/for_investors'
+        },
+    ]
     return (
         <div id='CreateMarket'>
             <div className="page_container">
+                <ul className="navbar_list">
+                    {
+                        links_object.map(v => {
+                            return (
+                                <li key={v.id} >
+                                    <Link to={v.path}>
+                                        {v.name}
+                                    </Link>
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
                 <div className="action_wrapper">
                     <h2>Create  a market for your models</h2>
                     <button className="btn_action animate_btn" style={{ '--btn-bg': '#E2761B' }}>
